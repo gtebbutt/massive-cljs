21a14197378ad1397ced4534b9a9cdef4e42ee8a
Greg Tebbutt <greg@sparrho.com>
2016-01-19 11:10:53 +0000

    Process kebab-case function names

--- src/massive_cljs/query.clj
+++ src/massive_cljs/query.clj
@@ -1,9 +1,15 @@
-(ns massive-cljs.query)
+(ns massive-cljs.query
+  (:require [clojure.string]))
+
+(defn camel-case
+  "Converts kebab-case to camelCase"
+  [s]
+  (clojure.string/replace s #"-(\w)" (comp clojure.string/upper-case second)))
 
 (defmacro db-fn
   [fn-name table params]
   `(let [channel (cljs.core.async/chan)]
-     (~(symbol (str "." (name fn-name))) (. (deref massive-cljs.core/instance) ~(symbol (str "-" (name table))))
+     (~(symbol (str "." (camel-case (name fn-name)))) (. (deref massive-cljs.core/instance) ~(symbol (str "-" (name table))))
        (cljs.core/clj->js ~params)
        (handler channel))
      channel))
