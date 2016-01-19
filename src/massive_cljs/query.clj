dd7011ffb6e231e42092f5acd946852a4e19685d
Greg Tebbutt <greg@sparrho.com>
2016-01-19 12:02:37 +0000

    Allow functions without a table argument to be called

--- src/massive_cljs/query.clj
+++ src/massive_cljs/query.clj
@@ -7,11 +7,14 @@
   (clojure.string/replace s #"-(\w)" (comp clojure.string/upper-case second)))
 
 (defmacro db-fn
-  [fn-name table params]
-  `(let [channel (cljs.core.async/chan)]
-     (-> (deref massive-cljs.core/instance)
-         (~(symbol (str ".-" (name table))))
-         (~(symbol (str "." (camel-case (name fn-name))))
-           (cljs.core/clj->js ~params)
-           (handler channel)))
-     channel))
+  ([fn-name params]
+   ;Hidden macro arguments &form &env must be passed explicitly
+   (db-fn &form &env fn-name nil params))
+  ([fn-name table params]
+   `(let [channel# (cljs.core.async/chan)]
+      (-> (deref massive-cljs.core/instance)
+          (~(if table (symbol (str ".-" (name table))) 'identity))
+          (~(symbol (str "." (camel-case (name fn-name))))
+            (cljs.core/clj->js ~params)
+            (handler channel#)))
+      channel#)))
